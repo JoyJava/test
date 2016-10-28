5a5e1f8138662fc6a40616d689ad3605b2ed4d94
fh63045@gmail.com <fh63045@gmail.com>
2016-10-28 18:09:38 +0800

    zk

---
+++ java8/src/main/java/zookeeper/ZKTest.java
@@ -0,0 +1,16 @@
+package zookeeper;
+
+/**
+ * User: fh
+ * Date: 16/10/28 下午2:52
+ */
+public class ZKTest {
+    public static void main(String[] args) {
+        CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy)
+
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3)
+        CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+        client.start();
+
+    }
+}
