92992e85c8f404ee517ae496cdbc434502afcc03
fenghong <fenghong@huobi.com>
2019-07-05 15:15:23 +0800

    jvm Test

--- java8/src/main/java/jvm/gc/Metaspace.java
+++ java8/src/main/java/jvm/gc/Metaspace.java
@@ -1,12 +1,15 @@
 package jvm.gc;
 
+import javassist.CannotCompileException;
+import scala.math.Ordering.String;
+
 /**
  * -XX:MaxMetaspaceSize=64m --> Caused by: java.lang.OutOfMemoryError: Metaspace
  */
 public class Metaspace {
     static javassist.ClassPool cp = javassist.ClassPool.getDefault();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws CannotCompileException {
         for (int i = 0; ; i++) {
             Class c = cp.makeClass("jvm.gc.MicroGeneratorHeapOOM" + i).toClass();
         }
